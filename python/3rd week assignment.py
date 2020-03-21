8c42d12d718c9e3f44ff9e63a53b88d17331af7f
peterkwon00 <61730841+peterkwon00@users.noreply.github.com>
2020-03-21 21:23:33 +0900

    Update 3rd week assignment.py

---
+++ python/3rd week assignment.py
@@ -0,0 +1,3 @@
+#body-content > div.newest-list > div > table > tbody > tr > td.number
+#body-content > div.newest-list > div > table > tbody > tr > td.info > a.title.ellipsis
+#body-content > div.newest-list > div > table > tbody > tr > td.info > a.artist.ellipsis
